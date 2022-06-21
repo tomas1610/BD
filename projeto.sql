79db3095d5c0eff7590d9e79a1e7c6c6c519138d
tomas1610 <tomasduarte1610@gmail.com>
2022-06-21 15:40:35 +0100

    DATA RI

--- projeto.sql
+++ projeto.sql
@@ -1,62 +1,63 @@
 CREATE DATABASE projeto 
 
-CREATE TABLE category(
-    nome varchar(255),
-    PRIMARY KEY(nome),
-    nome IN ((SELECT nome FROM simple_category) UNION (SELECT nome FROM super_category))
-);
-
-CREATE TABLE simple_category(
-    nome varchar(255),
-    FOREIGN KEY(nome) REFERENCES category(nome)
-);
-
-CREATE TABLE super_category(
-    nome varchar(255)
-    FOREIGN KEY(nome) REFERENCES category(nome)
-);
-
-CREATE TABLE tem_outra(
-    super_category,
-    category,
-    PRIMARY KEY(category),
-    FOREIGN KEY(category) REFERENCES category(nome),
-    FOREIGN KEY(super_category) REFERENCES super_category(nome),
-    super_category != category
-);
-
-CREATE TABLE product(
-    ean numeric(13,0),
-    cat varchar(255),
-    descr varchar(255),
-    PRIMARY KEY(ean),
-    FOREIGN KEY(cat) REFERENCES category(nome)
-);
-
-CREATE TABLE tem_categoria(
-    ean numeric(13,0),
-    nome varchar(255),
-    FOREIGN KEY(ean) REFERENCES product(ean),
-    FOREIGN KEY(nome) REFERENCES category(nome)
-);
-
-CREATE TABLE ivm(
-    num_serie int,
-    manuf varchar(255),
-    PRIMARY KEY(num_serie,manuf),
-);
-
-CREATE TABLE ponto_de_retalho(
-    nome varchar(255),
-    distrito varchar(255),
-    concelho varchar(255),
+CREATE TABLE categoria(
+    nome varchar(255) NOT NULL,
     PRIMARY KEY(nome)
 );
 
-CREATE TABLE installed_at(
-    num_serie int,
-    manuf varchar(255),
-    place varchar(255),
+CREATE TABLE categoria_simples(
+    nome varchar(255) NOT NULL,
+    PRIMARY KEY(nome),
+    FOREIGN KEY(nome) REFERENCES categoria(nome)
+);
+
+CREATE TABLE super_categoria(
+    nome varchar(255) NOT NULL,
+    PRIMARY KEY(nome),
+    FOREIGN KEY(nome) REFERENCES categoria(nome)
+);
+
+CREATE TABLE tem_outra(
+    super_categoria varchar(255) NOT NULL,
+    categoria varchar(255) NOT NULL,
+    PRIMARY KEY(categoria),
+    FOREIGN KEY(categoria) REFERENCES categoria(nome),
+    FOREIGN KEY(super_categoria) REFERENCES super_categoria(nome),
+);
+
+CREATE TABLE produto(
+    ean numeric(13,0) NOT NULL,
+    cat varchar(255) NOT NULL,
+    descr varchar(255) NOT NULL,
+    PRIMARY KEY(ean),
+    FOREIGN KEY(cat) REFERENCES categoria(nome)
+);
+
+CREATE TABLE tem_categoria(
+    ean numeric(13,0) NOT NULL,
+    nome varchar(255) NOT NULL,
+    FOREIGN KEY(ean) REFERENCES produto(ean),
+    FOREIGN KEY(nome) REFERENCES categoria(nome)
+);
+
+CREATE TABLE ivm(
+    num_serie int NOT NULL,
+    manuf varchar(255) NOT NULL,
+    PRIMARY KEY(num_serie),
+    PRIMARY KEY(manuf)
+);
+
+CREATE TABLE ponto_de_retalho(
+    nome varchar(255) NOT NULL,
+    distrito varchar(255) NOT NULL,
+    concelho varchar(255) NOT NULL, 
+    PRIMARY KEY(nome)
+);
+
+CREATE TABLE instalada_em(
+    num_serie int NOT NULL,
+    manuf varchar(255) NOT NULL,
+    place varchar(255) NOT NULL,
     PRIMARY KEY(num_serie),
     PRIMARY KEY(manuf),
     FOREIGN KEY(num_serie) REFERENCES ivm(num_serie),
@@ -64,76 +65,90 @@ CREATE TABLE installed_at(
     FOREIGN KEY(place) REFERENCES ponto_de_retalho(concelho)
 );
 
-CREATE TABLE shelve(
-    nro int,
-    num_serie int,
-    manuf varchar(255),
-    heigh int,
-    nome varchar(255),
+CREATE TABLE prateleira(
+    nro int NOT NULL,
+    num_serie int NOT NULL,
+    manuf varchar(255) NOT NULL,
+    heigh int NOT NULL,
+    nome varchar(255) NOT NULL,
     PRIMARY KEY(nro),
     PRIMARY KEY(num_serie),
     PRIMARY KEY(manuf),
     FOREIGN KEY(num_serie) REFERENCES ivm(num_serie),
     FOREIGN KEY(manuf) REFERENCES ivm(manuf),
-    FOREIGN KEY(nome) REFERENCES category(nome)
+    FOREIGN KEY(nome) REFERENCES categoria(nome)
 );
 
 CREATE TABLE planograma(
-    ean numeric(13,0),
-    nro int,
-    num_serie int,
-    manuf varchar(255),
-    faces int,
-    units int,
-    loc varchar(255)
+    ean numeric(13,0) NOT NULL,
+    nro int NOT NULL,
+    num_serie int NOT NULL,
+    manuf varchar(255) NOT NULL,
+    faces int NOT NULL,
+    units int NOT NULL,
+    PRIMARY KEY(ean),
+    PRIMARY KEY(nro),
+    PRIMARY KEY(num_serie),
+    PRIMARY KEY(manuf),
+    FOREIGN KEY(ean) REFERENCES produto(ean),
+    FOREIGN KEY(nro) REFERENCES prateleira(nro),
+    FOREIGN KEY(num_serie) REFERENCES prateleira(num_serie),
+    FOREIGN KEY(manuf) REFERENCES prateleira(manuf)
 );
 
-CREATE TABLE retailer(
-    tin int,
-    nome varchar(255),
+CREATE TABLE retalhista(
+    tin int NOT NULL,
+    nome varchar(255) NOT NULL,
     UNIQUE(nome),
     PRIMARY KEY(tin)
 );
 
-CREATE TABLE responsible_for(
-    nome_cat varchar(255),
-    tin int,
-    num_serie int,
-    manuf varchar(255),
-    PRIMARY KEY(num_serie,manuf),
+CREATE TABLE responsavel_por(
+    nome_cat varchar(255) NOT NULL,
+    tin int NOT NULL,
+    num_serie int NOT NULL,
+    manuf varchar(255) NOT NULL,
+    PRIMARY KEY(num_serie),
+    PRIMARY KEY(manuf),
     FOREIGN KEY(num_serie) REFERENCES ivm(num_serie),
     FOREIGN KEY(manuf) REFERENCES ivm(manuf),
-    FOREIGN KEY(tin) REFERENCES retailer(tin),
-    FOREIGN KEY(nome_cat) REFERENCES category(nome) 
+    FOREIGN KEY(tin) REFERENCES retalhista(tin),
+    FOREIGN KEY(nome_cat) REFERENCES categoria(nome) 
 );
 
-CREATE TABLE replanishment_event(
-    ean numeric(13,0),
-    nro int,
-    num_serie int,
-    manuf varchar(255),
-    instant varchar(10),
-    units int,
-    tin int,
-    PRIMARY KEY(ean,nro,num_serie,manuf,instant),
+CREATE TABLE evento_reposicao(
+    ean numeric(13,0) NOT NULL,
+    nro int NOT NULL,
+    num_serie int NOT NULL,
+    manuf varchar(255) NOT NULL,
+    instant varchar(10) NOT NULL,
+    units int NOT NULL,
+    tin int NOT NULL,
+    PRIMARY KEY(ean),
+    PRIMARY KEY(nro),
+    PRIMARY KEY(num_serie),
+    PRIMARY KEY(manuf),
+    PRIMARY KEY(instant),
     FOREIGN KEY(ean) REFERENCES planograma(ean),
     FOREIGN KEY(nro) REFERENCES planograma(nro),
     FOREIGN KEY(num_serie) REFERENCES planograma(num_serie),
     FOREIGN KEY(manuf) REFERENCES planograma(manuf),
-    FOREIGN KEY(tin) REFERENCES retailer(tin)
+    FOREIGN KEY(tin) REFERENCES retalhista(tin)
 );
+
+/*teste git*/
 
 /* RESTRIÇÔES DE INTEGRIDADE */
 
 DROP TRIGGER IF EXISTS verifica_categoria_dentro_mesma ON tem_outra;
-DROP TRIGGER IF EXISTS verifica_numero_unidades ON replanishment_event;
-DROP TRIGGER IF EXISTS verifica_produto_reposto ON replanishment_event;
+DROP TRIGGER IF EXISTS verifica_numero_unidades ON evento_reposicao;
+DROP TRIGGER IF EXISTS verifica_produto_reposto ON evento_reposicao;
 
 CREATE OR REPLACE FUNCTION verifica_numero_unidades_trigger_proc() RETURNS TRIGGER AS $$
 BEGIN
-    SELECT (replanishment_event.units, planograma.units) INTO (rep_units, plan_units)
-    FROM replanishment_event, planograma
-    WHERE replanishment_event.num_serie == planograma.num_serie;
+    SELECT (evento_reposicao.units, planograma.units) INTO (rep_units, plan_units)
+    FROM evento_reposicao, planograma
+    WHERE evento_reposicao.num_serie == planograma.num_serie;
     
     IF rep_units > place THEN
         RAISE EXCEPTION 'O número de unidades repostas no evento de reposição excedeu o número
@@ -144,15 +159,15 @@ $$ LANGUAGE plpgsql;
 
 
 CREATE OR REPLACE FUNCTION  verifica_categoria_dentro_mesma_trigger_proc() RETURNS TRIGGER AS $$
-DECLARE  in_category VARCHAR(255) := '' ;
+DECLARE  in_categoria VARCHAR(255) := '' ;
 DECLARE  super VARCHAR(255) := '' ;
 BEGIN
-    select (super,category) into (super,in_category) from tem_outra;
-    WHILE in_category IS NOT NULL THEN
-        IF in_category.nome == super.nome THEN
+    select (super,categoria) into (super,in_categoria) from tem_outra;
+    WHILE in_categoria IS NOT NULL THEN
+        IF in_categoria.nome == super.nome THEN
             RAISE EXCEPTION 'Uma Categoria não pode estar contida em si própria';
         END IF;
-        select (super,category) into (super,in_category) from tem_outra WHERE super.nome == in_category.nome;
+        select (super,categoria) into (super,in_categoria) from tem_outra WHERE super.nome == in_categoria.nome;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
@@ -164,17 +179,17 @@ DECLARE  cat_prateleira VARCHAR(255) := '' ;
 DECLARE  flag INTEGER := 0;
 BEGIN  
     select ean.cat,num_serie.nome INTO cat_produto,cat_prateleira 
-    from replanishment_event
+    from evento_reposicao
     
     WHILE cat_produto IS NOT NULL THEN
         select num_serie.nome INTO cat_prateleira 
-        from replanishment_event
+        from evento_reposicao
         WHILE cat_prateleira IS NOT NULL THEN
             IF cat_produto == cat_prateleira THEN
                 SET flag = flag + 1 ;
             END IF; 
-            select category into cat_prateleira from tem_outra where tem_outra.super.nome == cat_prateleira
-        select category into cat_produto from tem_outra where tem_outra.super.nome == cat_produto.nome /* no tem_outra.super é a coluna dos pais das super categorias ainda por definir o nome*/
+            select categoria into cat_prateleira from tem_outra where tem_outra.super.nome == cat_prateleira
+        select categoria into cat_produto from tem_outra where tem_outra.super.nome == cat_produto.nome /* no tem_outra.super é a coluna dos pais das super categorias ainda por definir o nome*/
         END LOOP;
     END LOOP;
     IF flag == 0 THEN
@@ -187,24 +202,24 @@ END;
 
 
 SELECT MAX(count(nome)),
-FROM responsible_for
+FROM responsavel_por
 NATURAL JOIN retailer
 GROUP BY nome_cat
 
  
 SELECT nome
-FROM responsible_for NATURAL JOIN simple_category
+FROM responsavel_por NATURAL JOIN categoria_simples
 GROUP BY nome
-HAVING count(distinct (simple_category.nome)) = (SELECT count(*) FROM simple_category);
+HAVING count(distinct (categoria_simples.nome)) = (SELECT count(*) FROM categoria_simples);
 
 SELECT ean 
-FROM product
-WHERE product.ean NOT IN (SELECT ean from replanishment_event)
+FROM produto
+WHERE produto.ean NOT IN (SELECT ean from evento_reposicao)
 
 
 SELECT  nome
-FROM replanishment_event
-NATURAL JOIN product,retailer
+FROM evento_reposicao
+NATURAL JOIN produto,retailer
 GROUP BY ean
 WHERE (count(distinct nome) == 1);
  
